91300d43a8d98ba204529bce4e2f6201b74dc722
Jean-yun <yesirchloe@gmail.com>
2024-07-19 16:34:20 +0900

    Initial Commit

---
+++ BizNews/BizNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  BizNewsApp.swift
+//  BizNews
+//
+//  Created by 윤진영 on 7/19/24.
+//
+
+import SwiftUI
+
+@main
+struct BizNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
